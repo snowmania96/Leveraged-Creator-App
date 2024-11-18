b5585b422a0e6e4193eff344d350af77c2f46a7d
snowmania96 <blatomini@gamil.com>
2024-11-18 19:46:22 +0100

    completed

--- src/TradeInForm.js
+++ src/TradeInForm.js
@@ -2,13 +2,6 @@ import { useState, useEffect } from "react";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { Label } from "./components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./components/ui/select";
 
 import axios from "axios";
 
@@ -45,10 +38,9 @@ export default function TradeInForm() {
   const fetchYears = async () => {
     try {
       await api.get("/year").then(function (response) {
-        let years = ["Select Years"]
-        years.push(...response.data.years)
-        console.log(years)
-        setYears(years);
+        const year = ["Select Years", ...response.data.years];
+        console.log(year);
+        setYears(year);
       });
     } catch (error) {
       console.log("Error fetching 'years' data", error);
@@ -59,7 +51,7 @@ export default function TradeInForm() {
     try {
       await api.get(`/make/${year}`).then(function (response) {
         let makes = ["Select Makes"];
-        makes.push(...response.data.makes)
+        makes.push(...response.data.makes);
         setMakes(makes);
       });
     } catch (error) {
@@ -70,8 +62,8 @@ export default function TradeInForm() {
   const fetchModels = async (year, make) => {
     try {
       await api.get(`/model/${year}/${make}`).then(function (response) {
-        let models = ["Select Models"]
-        models.push(...response.data.models)
+        let models = ["Select Models"];
+        models.push(...response.data.models);
         setModels(models);
       });
     } catch (error) {
@@ -80,7 +72,7 @@ export default function TradeInForm() {
   };
 
   useEffect(() => {
-    fetchYears()
+    fetchYears();
   }, []);
 
   useEffect(() => {
@@ -126,6 +118,7 @@ export default function TradeInForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log(years);
     setError("");
     setIsLoading(true);
     if (step === 1) {
@@ -154,7 +147,9 @@ export default function TradeInForm() {
           setStep(1);
         } catch (error) {
           console.error("Error:", error);
-          setError("There was an error submitting your request. Please try again.");
+          setError(
+            "There was an error submitting your request. Please try again."
+          );
         }
       }
     }
@@ -162,86 +157,74 @@ export default function TradeInForm() {
   };
 
   return (
-    <div className="max-w-md mx-auto p-5 bg-white rounded-lg shadow-md">
+    <div className="max-w-md mt-32 mx-auto p-5 bg-white rounded-lg shadow-md">
       {error && <div className="text-red-600 mb-4">{error}</div>}
       <form onSubmit={handleSubmit} className="space-y-4">
         {step === 1 ? (
           <>
             <div className="space-y-2">
               <Label htmlFor="year">Year</Label>
-              <Select
+              <select
                 name="year"
+                className="w-full p-1"
                 value={formData.year}
-                onValueChange={(value) => handleSelectChange("year", value)}
-              >
-                <SelectTrigger id="year">
-                  <SelectValue placeholder="Select Year" />
-                </SelectTrigger>
-                <SelectContent>
-                  {years.map((year) => (
-                    <SelectItem key={year} value={year}>
-                      {year}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                onChange={(e) => handleSelectChange("year", e.target.value)}>
+                {years.map((year) => (
+                  <option
+                    value={year}
+                    className="text-sm text-gray-700 hover:bg-gray-100 w-full text-center">
+                    {year}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="space-y-2">
               <Label htmlFor="make">Make</Label>
-              <Select
+              <select
                 name="make"
+                className="w-full p-1"
                 value={formData.make}
-                onValueChange={(value) => handleSelectChange("make", value)}
-              >
-                <SelectTrigger id="make">
-                  <SelectValue placeholder="Select Make" />
-                </SelectTrigger>
-                <SelectContent>
-                  {makes.map((make) => (
-                    <SelectItem key={make} value={make}>
-                      {make}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                onChange={(e) => handleSelectChange("make", e.target.value)}>
+                {makes.map((make) => (
+                  <option
+                    value={make}
+                    className="text-sm text-gray-700 hover:bg-gray-100 w-full text-center">
+                    {make}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="space-y-2">
               <Label htmlFor="model">Model</Label>
-              <Select
+              <select
                 name="model"
+                className="w-full p-1"
                 value={formData.model}
-                onValueChange={(value) => handleSelectChange("model", value)}
-              >
-                <SelectTrigger id="model">
-                  <SelectValue placeholder="Select Model" />
-                </SelectTrigger>
-                <SelectContent>
-                  {models.map((model) => (
-                    <SelectItem key={model} value={model}>
-                      {model}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                onChange={(e) => handleSelectChange("model", e.target.value)}>
+                {models.map((model) => (
+                  <option
+                    value={model}
+                    className="text-sm text-gray-700 hover:bg-gray-100 w-full text-center">
+                    {model}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="space-y-2">
               <Label htmlFor="state">State</Label>
-              <Select
+              <select
                 name="state"
+                className="w-full p-1"
                 value={formData.state}
-                onValueChange={(value) => handleSelectChange("state", value)}
-              >
-                <SelectTrigger id="state">
-                  <SelectValue placeholder="Select State" />
-                </SelectTrigger>
-                <SelectContent>
-                  {states.map((state) => (
-                    <SelectItem key={state} value={state}>
-                      {state}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+                onChange={(e) => handleSelectChange("state", e.target.value)}>
+                {states.map((state) => (
+                  <option
+                    value={state}
+                    className="text-sm text-gray-700 hover:bg-gray-100 w-full text-center">
+                    {state}
+                  </option>
+                ))}
+              </select>
             </div>
             <div className="space-y-2">
               <Label htmlFor="miles">Miles</Label>
@@ -254,7 +237,7 @@ export default function TradeInForm() {
                 placeholder="Enter miles"
               />
             </div>
-            <Button type="submit" className="w-full" disabled={isLoading}>
+            <Button type="submit" className="w-full p-1" abled={isLoading}>
               {isLoading ? "Loading..." : "Next"}
             </Button>
           </>
@@ -293,12 +276,12 @@ export default function TradeInForm() {
                 placeholder="Enter your phone number"
               />
             </div>
-            <Button type="submit" className="w-full" disabled={isLoading}>
+            <Button type="submit" className="w-full p-1" abled={isLoading}>
               {isLoading ? "Submitting..." : "Get My Trade-In Value"}
             </Button>
             <p className="text-sm text-red-600 mt-2">
-              Warning! Make sure your information is correct because we will text you the final
-              report!
+              Warning! Make sure your information is correct because we will
+              text you the final report!
             </p>
           </>
         )}
--- src/components/ui/label.js
+++ src/components/ui/label.js
@@ -1,6 +1,8 @@
 export function Label({ htmlFor, children }) {
   return (
-    <label htmlFor={htmlFor} className="block text-sm font-medium text-gray-700">
+    <label
+      htmlFor={htmlFor}
+      className="block text-left pl-3 text-sm font-medium text-gray-700">
       {children}
     </label>
   );
